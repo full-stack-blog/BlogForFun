6516bd594e89fd9eac4b1dac6be28ef107f8321d
Paris Tyus <Tyus.Paris1@gmail.com>
2020-06-11 10:25:47 -0500

    setting up controller

--- src/main/java/com/example/blog/Repositories/UserRepo.java
+++ src/main/java/com/example/blog/Repositories/UserRepo.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    User findByUserRole(String users);
+    User findUserById(long id);
+
+
 
 }
