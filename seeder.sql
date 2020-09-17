96fe701a86459020565f451a594232ad4be4cbfe
Paris Tyus <Tyus.Paris1@gmail.com>
2020-09-17 13:16:13 -0500

    changed formatting for edit profile

--- seeder.sql
+++ seeder.sql
@@ -2,7 +2,8 @@ USE BlogForFun_db;
 
 -- Will not seed if no user exists in user table!!!!!
 
-
+insert into user (user_role, email, username, password, first_name, last_name, profile_image, cover_img) values
+('admin','Ptyus31@gmail.com','Ptyus','P123','Paris','Tyus', 'https://images.pexels.com/photos/1034675/pexels-photo-1034675.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'https://images.pexels.com/photos/1031955/pexels-photo-1031955.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 
 insert into categories(name) values
 ('Sports'),
